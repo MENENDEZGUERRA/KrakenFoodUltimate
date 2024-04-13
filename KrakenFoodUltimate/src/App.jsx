218457693522ca34bbb66bd4962825d75c851bee
MENENDEZGUERRA <menendezguerrajuan@gmail.com>
2024-04-13 13:13:47 -0600

    Added Logo component

--- KrakenFoodUltimate/src/App.jsx
+++ KrakenFoodUltimate/src/App.jsx
@@ -4,12 +4,14 @@ import './App.css'
 import UserList from './UserList'
 import AddUser from './AddUser'
 import Login from './Login'
+import Logo from './Logo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Logo />
     <AddUser />
     <Login />
     <UserList />
---
+++ KrakenFoodUltimate/src/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+      <h1>Kraken Food Ultimate</h1>
+      <span role="img" aria-label="squid" style={{ fontSize: '60px' }}>
+        🦑
+      </span>
+    </div>
+  );
+}
+
+export default Logo;
